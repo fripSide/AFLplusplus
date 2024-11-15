12091809b742f94912e2d67b9e6b1d468e6491eb
fripSide <fripSide@users.noreply.github.com>
2024-11-15 22:31:37 +0800

    Fix the issue#2244
    Address the [issue#2244](https://github.com/AFLplusplus/AFLplusplus/issues/2244) to ensure that `argv_fuzz_persistent_demo.c` can be successfully compiled without any error messages

--- utils/argv_fuzzing/argv_fuzz_persistent_demo.c
+++ utils/argv_fuzzing/argv_fuzz_persistent_demo.c
@@ -7,6 +7,21 @@ using persistent mode.
 #include <string.h>
 #include "argv-fuzz-inl.h"
 
+/* this lets the source compile without afl-clang-fast/lto */
+#ifndef __AFL_FUZZ_TESTCASE_LEN
+
+  ssize_t       fuzz_len;
+  unsigned char fuzz_buf[1024000];
+
+  #define __AFL_FUZZ_TESTCASE_LEN fuzz_len
+  #define __AFL_FUZZ_TESTCASE_BUF fuzz_buf
+  #define __AFL_FUZZ_INIT() void sync(void);
+  #define __AFL_LOOP(x) \
+    ((fuzz_len = read(0, fuzz_buf, sizeof(fuzz_buf))) > 0 ? 1 : 0)
+  #define __AFL_INIT() sync()
+
+#endif
+
 __AFL_FUZZ_INIT();
 
 /* The main function is an entry point for a program.
